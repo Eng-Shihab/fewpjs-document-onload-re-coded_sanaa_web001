0738ea0f601945033a7d42e7c34e241522bcc895
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-07-19 23:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  changText();
+});
+
+function changText()
+{
+let text = document.getElementById("text");
+text.innerHTML ="This is really cool!";
+
+}
